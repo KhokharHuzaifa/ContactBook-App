530dbf56bb9385759d9f5107f5e5a221b1de007f
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-03 05:42:13 +0500

    Store updated

--- src/Pages/OutletRoute.jsx
+++ src/Pages/OutletRoute.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import Navbar from "./Navbar"
+import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
 const OutletRoute = () => {
   return (
