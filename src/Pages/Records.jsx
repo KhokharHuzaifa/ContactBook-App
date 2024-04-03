7c2c80cba2eeb09c5c67d67a73bc1d850c4d8ebd
Huzaifa <creativehuzaifa97@gmail.com>
2024-04-03 05:42:46 +0500

    Contact book updated

--- src/Pages/Records.jsx
+++ src/Pages/Records.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from "react";
-import Modal from "./Modal";
+import Modal from "../Components/Modal";
 
 const Records = () => {
 
